89691e2016c8ca07c86a4bae76cb2f3b0beffcbd
Kateryna Solodilova <kiivikat@gmail.com>
2023-03-16 14:23:48 +0100

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import { InfinitySpin } from 'react-loader-spinner';
 export const Loader = () => {
   return (
     <div>
-      <InfinitySpin width="200" color="#fc0373" />;
+      <InfinitySpin width="200" color="#fc0373" />
     </div>
   );
 };
